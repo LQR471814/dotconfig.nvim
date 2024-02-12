92cd9c99a1d60e6c55417b159566fee9c8ae0042
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-02-11 16:06:55 -0800

    fix input delay after pressing space

--- lua/lqr471814/plugins/lsp.lua
+++ lua/lqr471814/plugins/lsp.lua
@@ -60,7 +60,7 @@ return {
                     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
                     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
                     vim.keymap.set({ "n", "v" }, "<space>.", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set({ "n", "x", "i" }, "<leader>f", function()
+                    vim.keymap.set({ "n" }, "<leader>f", function()
                         vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
                     end, opts)
                 end,
