abcb8594c8b8ff2e52f0d1ec28f4d14620044576
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-05-31 10:27:46 -0700

    organize imports on save for golang

--- lua/lqr471814/plugins/lsp.lua
+++ lua/lqr471814/plugins/lsp.lua
@@ -58,6 +58,27 @@ return {
                 end,
             })
 
+            local golang_organize_imports = function(bufnr, isPreflight)
+                local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(bufnr))
+                params.context = { only = { "source.organizeImports" } }
+
+                if isPreflight then
+                    vim.lsp.buf_request(bufnr, "textDocument/codeAction", params, function() end)
+                    return
+                end
+
+                local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 3000)
+                for _, res in pairs(result or {}) do
+                    for _, r in pairs(res.result or {}) do
+                        if r.edit then
+                            vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(bufnr))
+                        else
+                            vim.lsp.buf.execute_command(r.command)
+                        end
+                    end
+                end
+            end
+
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(ev)
@@ -74,6 +95,21 @@ return {
                     end, opts)
                     vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
                     vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+
+                    local bufnr = ev.buf
+                    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                    if client and client.name == "gopls" then
+                        -- hack: Preflight async request to gopls, which can prevent blocking when save buffer on first time opened
+                        golang_organize_imports(bufnr, true)
+
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            pattern = "*.go",
+                            group = vim.api.nvim_create_augroup("LspGolangOrganizeImports." .. bufnr, {}),
+                            callback = function()
+                                golang_organize_imports(bufnr)
+                            end,
+                        })
+                    end
                 end,
             })
 
