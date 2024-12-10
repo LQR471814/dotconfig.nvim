2172b1c0c7e75217655cf84fed659cd4656bedbe
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-12-09 16:42:11 -0800

    add more latex snippets, enable concealer

--- lua/lqr471814/set.lua
+++ lua/lqr471814/set.lua
@@ -77,3 +77,5 @@ vim.keymap.set("n", "<leader>no", "<CMD>Neorg return<CR>")
 
 vim.g.vimtex_view_method = "zathura"
 vim.g.tex_flavor = "latex"
+vim.opt.conceallevel = 1
+vim.g.tex_conceal = "abdmg"
--- snippets/tex/core.lua
+++ snippets/tex/core.lua
@@ -1,10 +1,12 @@
 local ls = require('luasnip')
 local s = ls.snippet
 local i = ls.insert_node
+local f = ls.function_node
 local fmta = require('luasnip.extras.fmt').fmta
 local rep = require('luasnip.extras').rep
 
 return {
+    -- \begin{} \end{}
     s({ trig = "begi", snippetType = "autosnippet" }, fmta(
         [[
         \begin{<>}
@@ -17,10 +19,14 @@ return {
             rep(1),
         }
     )),
+
+    -- inline mode math
     s({ trig = "mk", snippetType = "autosnippet" }, fmta(
         "$<>$ ",
         { i(1) }
     )),
+
+    -- display mode math
     s({ trig = "dm", snippetType = "autosnippet" }, fmta(
         [[
         \[
@@ -29,12 +35,38 @@ return {
         ]],
         { i(1) }
     )),
+
+    -- usepackage
     s({ trig = "usep", snippetType = "autosnippet" }, fmta(
         "\\usepackage{<>}",
         { i(1) }
     )),
+
+    -- integrals
     s({ trig = "int", snippetType = "autosnippet" }, fmta(
-        "\\int_{<>}^{<>} ",
+        "\\int_{<>}^{<>}",
         { i(1), i(2) }
-    ))
+    )),
+
+    -- subscript
+    s({ trig = "([a-zA-Z])_", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
+        "<>_{<>}",
+        { f(function(_, snip) return snip.captures[1] end), i(1) }
+    )),
+
+    -- superscript
+    s({ trig = "([a-zA-Z])^", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
+        "<>^{<>}",
+        { f(function(_, snip) return snip.captures[1] end), i(1) }
+    )),
+
+    -- fraction
+    s({ trig = "//", regTrig = true, snippetType = "autosnippet" }, fmta(
+        "\\frac{<>}{<>}",
+        { i(1), i(2) }
+    )),
+    s({ trig = "([a-zA-Z%d_%^-%+%*%%{}%(%)]+)/", regTrig = true, snippetType = "autosnippet" }, fmta(
+        "\\frac{<>}{<>}",
+        { f(function(_, snip) return snip.captures[1] end), i(1) }
+    )),
 }
