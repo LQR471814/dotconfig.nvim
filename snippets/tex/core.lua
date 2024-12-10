6f1cc945d2e59feada64e13fcb4c7ad0df321eef
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-12-09 19:50:11 -0800

    add more snippets

--- snippets/tex/core.lua
+++ snippets/tex/core.lua
@@ -1,13 +1,19 @@
 local ls = require('luasnip')
 local s = ls.snippet
+local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local fmta = require('luasnip.extras.fmt').fmta
 local rep = require('luasnip.extras').rep
 
 return {
-    -- \begin{} \end{}
-    s({ trig = "begi", snippetType = "autosnippet" }, fmta(
+    -- \documentclass
+    s({ trig = "\\doc", snippetType = "autosnippet" }, {
+        t("\\documentclass[a4paper, 12pt]{article}")
+    }),
+
+    -- \begin \end
+    s({ trig = "\\beg", snippetType = "autosnippet" }, fmta(
         [[
         \begin{<>}
           <>
@@ -18,6 +24,20 @@ return {
             i(2),
             rep(1),
         }
+    )),
+
+    -- \sections
+    s({ trig = "\\sec", snippetType = "autosnippet" }, fmta(
+        "\\section{<>}",
+        { i(1) }
+    )),
+    s({ trig = "\\ssec", snippetType = "autosnippet" }, fmta(
+        "\\subsection{<>}",
+        { i(1) }
+    )),
+    s({ trig = "\\sssec", snippetType = "autosnippet" }, fmta(
+        "\\subsubsection{<>}",
+        { i(1) }
     )),
 
     -- inline mode math
@@ -43,29 +63,34 @@ return {
     )),
 
     -- integrals
-    s({ trig = "int", snippetType = "autosnippet" }, fmta(
+    s({ trig = "int", snippetType = "autosnippet" }, t("\\int")),
+    s({ trig = "dint", snippetType = "autosnippet" }, fmta(
         "\\int_{<>}^{<>}",
         { i(1), i(2) }
     )),
 
     -- subscript
-    s({ trig = "([a-zA-Z])_", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
+    s({ trig = "([a-zA-Z])(%d)", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
+        "<>_<>",
+        { f(function(_, snip) return snip.captures[1] end), f(function(_, snip) return snip.captures[2] end) }
+    )),
+    s({ trig = "([a-zA-Z]+)_", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
         "<>_{<>}",
         { f(function(_, snip) return snip.captures[1] end), i(1) }
     )),
 
     -- superscript
-    s({ trig = "([a-zA-Z])^", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
+    s({ trig = "([a-zA-Z]+)^", regTrig = true, wordTrig = false, snippetType = "autosnippet" }, fmta(
         "<>^{<>}",
         { f(function(_, snip) return snip.captures[1] end), i(1) }
     )),
 
     -- fraction
-    s({ trig = "//", regTrig = true, snippetType = "autosnippet" }, fmta(
+    s({ trig = "//", snippetType = "autosnippet" }, fmta(
         "\\frac{<>}{<>}",
         { i(1), i(2) }
     )),
-    s({ trig = "([a-zA-Z%d_%^-%+%*%%{}%(%)]+)/", regTrig = true, snippetType = "autosnippet" }, fmta(
+    s({ trig = "([a-zA-Z%d_%^-%+%*%}%)]+)/", regTrig = true, snippetType = "autosnippet" }, fmta(
         "\\frac{<>}{<>}",
         { f(function(_, snip) return snip.captures[1] end), i(1) }
     )),
