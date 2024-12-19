1055d23356c82541b0441a5560bc6f33f056e424
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-12-19 11:00:44 -0800

    update proto snippets

---
+++ snippets/proto.lua
@@ -0,0 +1,35 @@
+local ls = require('luasnip')
+local s = ls.snippet
+local i = ls.insert_node
+local fmta = require('luasnip.extras.fmt').fmta
+local rep = require('luasnip.extras').rep
+
+return {
+    s("reqres", fmta(
+        [[
+        // <>
+        message <>Request {
+            <>
+        }
+        message <>Response {
+            <>
+        }
+        ]],
+        {
+            i(1),
+            rep(1),
+            i(2),
+            rep(1),
+            i(3)
+        }
+    )),
+    s("rpc", fmta(
+        "rpc <>(<>Request) returns (<>Response);",
+        {
+            i(1),
+            rep(1),
+            rep(1),
+        }
+    )),
+}
+
