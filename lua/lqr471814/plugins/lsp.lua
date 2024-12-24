0679c8edc1643aa5fc4c54973e586ce954da2f2d
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-12-24 10:42:52 -0800

    disable tab completion in nvim-cmp

--- lua/lqr471814/plugins/lsp.lua
+++ lua/lqr471814/plugins/lsp.lua
@@ -157,25 +157,27 @@ return {
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<Tab>'] = cmp.mapping(function(fallback)
-                        if luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        elseif cmp.visible() then
-                            cmp.select_prev_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
                     ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+
+                    -- disable tab completion cause it does some weird things
+                    -- ['<Tab>'] = cmp.mapping(function(fallback)
+                    --     if luasnip.expand_or_jumpable() then
+                    --         luasnip.expand_or_jump()
+                    --     elseif cmp.visible() then
+                    --         cmp.select_next_item()
+                    --     else
+                    --         fallback()
+                    --     end
+                    -- end, { "i", "s" }),
+                    -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    --     if luasnip.jumpable(-1) then
+                    --         luasnip.jump(-1)
+                    --     elseif cmp.visible() then
+                    --         cmp.select_prev_item()
+                    --     else
+                    --         fallback()
+                    --     end
+                    -- end, { "i", "s" }),
                 }),
             })
         end
